0e083c1edf2b23f13764bcc271659df0c87f3ad5
bawanglb <bawanglb@163.com>
2016-12-08 16:23:15 +0800

    first commit

---
+++ app/src/main/java/com/example/sxy/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package  com.example.sxy.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView.setText("testview show");
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "textview click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Log.d(TAG, "onCreate: ");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+}
